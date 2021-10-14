eeac38e6f1f2662e9b2a9cdb7b7d0a0b7ef6b5be
Noah Applegate <noahapplegate1@gmail.com>
2021-10-13 17:19:27 -0700

    Basic remove button functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,9 @@
 const MIN_PLAYERS = 1;
 const MAX_PLAYERS = 3;
 
-// Store score card element names
+// Store score card and remove button DOM element names
 const scoreCardNames = ['score-card1', 'score-card2', 'score-card3'];
+const removeButtonNames = ['remove-button1', 'remove-button2', 'remove-button3'];
 
 // Get references to score card DOM elements
 // Only show score cards for min players to start
@@ -16,6 +17,12 @@ for (let i = MAX_PLAYERS-1; i >= MIN_PLAYERS; i--) {
     hiddenCard.hidden = true;
     hiddenCards.push(hiddenCard);
 }
+
+// Get references to remove button DOM elements
+const removeButtons = []
+removeButtonNames.forEach((buttonName) => {
+    removeButtons.push(document.getElementById(buttonName));
+})
 
 // Get add button DOM element
 let addButton = document.getElementById('add-button');
@@ -35,3 +42,23 @@ addButton.addEventListener('click', () => {
         }
     }
 });
+
+// Add Event listeners and handlers for closing
+const removeScoreCard = (event) => {
+    if (displayedCards.length > MIN_PLAYERS) {
+        let scoreCard = event.currentTarget.parentNode;
+        const index = displayedCards.indexOf(scoreCard);
+        if (index > -1) {
+            if (displayedCards.length == MAX_PLAYERS) {
+                addCard.hidden = false;
+            }
+
+            displayedCards.splice(index, 1);
+            scoreCard.hidden = true;
+            hiddenCards.push(scoreCard);
+        }
+    }
+}
+removeButtons.forEach((removeButton) => {
+    removeButton.addEventListener('click', removeScoreCard);
+})
