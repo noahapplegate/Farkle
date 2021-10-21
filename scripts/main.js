79f38dc567ef9adfc1e54a4ec3829175bc7a09d3
timjancic <timjancic.gravity@gmail.com>
2021-10-20 23:06:44 -0500

    Added checkScore function which gives results based on input dice results and added a scoreTable object for reference. Roll button has temporary functionality

--- scripts/main.js
+++ scripts/main.js
@@ -85,9 +85,144 @@ document.getElementById("play-b-6")];
 
 // Event listener and handler for clicking roll button
 rollB.addEventListener('click', () => {
-
+  console.log(checkScore(rollDice()));
 });
 
+const scoreTable = {
+  farkle: ["farkle", 0],
+  one: ["1", 100], //0
+  five: ["5", 50], //1
+  three1s: ["three 1s", 1000],
+  three2s: ["three 2s", 200],
+  three3s: ["three 3s", 300],
+  three4s: ["three 4s", 400],
+  three5s: ["three 5s", 500],
+  three6s: ["three 6s", 600],
+  fourKind: ["four of a kind", 1000],
+  fiveKind: ["five of a kind", 2000],
+  sixKind: ["six of a kind", 3000],
+  straight: ["straight", 1500],
+  threePairs: ["three pairs", 1500],
+  fourKindPair: ["four of kind + pair", 1500],
+  twoTriplets: ["two triplets", 2500]
+};
+
+function checkScore(d6Array) {
+  /*
+  This function takes in an array of 6 numbers and assigns the results
+  variable with pairs of combination names and scores depending on the
+  numbers in the given arrays.
+  This function needs access to global object "scoreTable"
+  */
+  let results = [];
+
+  let nOne = d6Array.filter(number => number == 1).length;
+  let nTwo = d6Array.filter(number => number == 2).length;
+  let nThree = d6Array.filter(number => number == 3).length;
+  let nFour = d6Array.filter(number => number == 4).length;
+  let nFive = d6Array.filter(number => number == 5).length;
+  let nSix = d6Array.filter(number => number == 6).length;
+
+  let nArray = [nOne, nTwo, nThree, nFour, nFive, nSix];
+
+  /*THE BELOW IF STATEMENTS descend in priority. This structure assumes
+  that higher value combinations got caught above and returned*/
+
+  //check for straight by seeing if each number appears once
+  if (nArray.every(number => number == 1)) {
+    results.push(scoreTable.straight);
+    return results; //return since there are no better melds
+  }
+  //check for six of a kind by seeing if any length equals 6
+  if (nArray.some(number => number == 6)) {
+    results.push(scoreTable.sixKind);
+    return results;
+  }
+  //check for two triplets
+  if (nArray.filter(number => number == 3).length == 2) {
+    results.push(scoreTable.twoTriplets);
+    return results;
+  }
+  //check for three pairs
+  if (nArray.filter(number => number == 2).length == 3) {
+    results.push(scoreTable.threePairs);
+    return results;
+  }
+  //check for five of a kind and extra 1 or 5
+  if (nArray.some(number => number == 5)) {
+    results.push(scoreTable.fiveKind);
+    if (nOne == 1) {
+      results.push(scoreTable.one);
+    }
+    if (nFive == 1) {
+      results.push(scoreTable.five);
+    }
+    return results;
+  }
+  //check for four of a kind and four of a kind + pair
+  if (nArray.some(number => number == 4)) {
+    //check for also having a pair
+    if (nArray.some(number => number == 2)) {
+      results.push(scoreTable.fourKindPair)
+      return results;
+    }
+
+    if (nOne == 4) {
+      //if four 1s matching 1-1-1 and 1 is better than four of a kind
+      results.push(scoreTable.three1s);
+      results.push(scoreTable.one);
+    } else {
+      results.push(scoreTable.fourKind);
+    }
+    //check for stray 1 or 5
+    if (nOne == 1) {
+      results.push(scoreTable.one);
+    }
+    if (nFive == 1) {
+      results.push(scoreTable.five);
+    }
+    return results;
+  }
+  //check for remaining combinations
+  for (let i = 0; i < 6; i++) {
+    if (nArray[i] == 3) {
+      switch (i + 1) {
+        case 1:
+          results.push(scoreTable.three1s);
+          nOne = nOne - 3;
+          break;
+        case 2:
+          results.push(scoreTable.three2s);
+          break;
+        case 3:
+          results.push(scoreTable.three3s);
+          break;
+        case 4:
+          results.push(scoreTable.three4s);
+          break;
+        case 5:
+          results.push(scoreTable.three5s);
+          nFive = nFive - 3;
+          break;
+        case 6:
+          results.push(scoreTable.three6s);
+          break;
+      }
+    }
+  }
+  //add 1s or 5s depending on how many there are (there shouldn't be more than 2)
+  for (let i = 0; i < nOne; i++) {
+    results.push(scoreTable.one);
+  }
+  for (let i = 0; i < nFive; i++) {
+    results.push(scoreTable.five);
+  }
+  //if no other results have been pushed then you farkled!
+  if (results.length == 0) {
+    results.push(scoreTable.farkle);
+  }
+  return results;
+}
 
 function rollDice(nDice = 6) {
   /*
